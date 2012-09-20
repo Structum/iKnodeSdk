6e4792928ca2ae4ea2c03e55515ea29dd1f56c19
Jorge Medina <jge.mmtz@gmail.com>
2012-09-19 22:59:22 -0500

    Added the android library

---
+++ Android/src/net/structum/iknodesdk/Task.java
@@ -0,0 +1,29 @@
+package net.structum.iknodesdk;
+
+/**
+ * 
+ * @author jgemedina
+ *
+ */
+public final class Task<T> {
+	/**
+	 * 
+	 */
+	private Runnable _runnable;
+	
+	/**
+	 * 
+	 * @param r
+	 */
+	public Task(Runnable r) {
+		this._runnable = r;
+	}
+		
+	/**
+	 * 
+	 */
+	public void execute() {
+		Thread t = new Thread(this._runnable);
+		t.start();
+	}
+}
---
+++ Android/src/net/structum/iknodesdk/iKnodeClientException.java
@@ -0,0 +1,61 @@
+package net.structum.iknodesdk;
+
+/**
+ * Defines a generic Exception type.
+ * 
+ * @author jgemedina
+ * @version 0.1
+ */
+public final class iKnodeClientException extends Exception {
+
+	/**
+	 *  Class Serial Id.
+	 *  
+	 *  @since 0.1
+	 */
+	private static final long serialVersionUID = -3062368242694621398L;
+
+	/**
+	 * Creates a new instance of the {@link iKnodeClientException} class.
+	 * 
+	 * @since 0.1
+	 */
+	public iKnodeClientException()
+	{
+		super();
+	}
+	
+	/**
+	 * Creates a new instance of the {@link iKnodeClientException} class.
+	 * 
+	 * @param message Exception message.
+	 * @since 0.1
+	 */
+	public iKnodeClientException(final String message)
+	{
+		super(message);
+	}
+	
+	/**
+	 * Creates a new instance of the {@link iKnodeClientException} class.
+	 * 
+	 * @param thr Inner exception.
+	 * @since 0.1
+	 */
+	public iKnodeClientException(final Throwable thr)
+	{
+		super(thr);
+	}
+	
+	/**
+	 * Creates a new instance of the {@link iKnodeClientException} class.
+	 * 
+	 * @param message Exception message.
+	 * @param thr Inner exception.
+	 * @since 0.1
+	 */
+	public iKnodeClientException(final String message, final Throwable thr)
+	{
+		super(message, thr);
+	}
+}
